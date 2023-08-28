93afad0f90876c8074a8ad4aad503794fa74e544
Agustin Lobos <aguslobos.1712@gmail.com>
2023-08-28 19:57:30 -0300

    Generamos las apuestas pero faltan detalles, creamos la funcion sigJug y le faltan detalles.

--- src/Jugadores.java
+++ src/Jugadores.java
@@ -1,15 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Jugadores {
 
     private String nombre;
 
     private int dinero;
 
+    private Set<Carta> mano;
+
+
     public Jugadores() {
+        this.mano = new HashSet<>();
     }
 
     public Jugadores(String nombre, int dinero) {
         this.nombre = nombre;
         this.dinero = dinero;
+        this.mano = new HashSet<>();
+    }
+
+    public Set<Carta> getMano() {
+        return mano;
+    }
+
+    public void setMano(Set<Carta> mano) {
+        this.mano = mano;
     }
 
     public String getNombre() {
@@ -26,5 +42,9 @@ public class Jugadores {
 
     public void setDinero(int dinero) {
         this.dinero = dinero;
+    }
+
+    public void agregarMano(Carta carta){
+        mano.add(carta);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
     private static List<Carta> cartasEnBaraja = new ArrayList<>();
-
+    private static Scanner in = new Scanner(System.in).useDelimiter("\n");
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in).useDelimiter("\n");
         baraja();
         System.out.println("BIENVENIDO A BLACKJACK!");
-        System.out.print("Ingrese su nombre a continuacion: \n->");
-        String nombre = scan.next();
-        Jugadores j1 = new Jugadores(nombre, 25000); //Creamos el jugador con el nombre antes ingresado
+        System.out.print("Ingrese su nombre: \n->");
+        String nombre = in.next();
+        Jugadores j1 = new Jugadores(nombre, 25000); // Creamos el jugador con el nombre antes ingresado
+        Jugadores cru = new Jugadores("crupier", 1147483647);// Creamos al crupier
         j1.setNombre(j1.getNombre().toUpperCase());
         System.out.println("BIENVENIDO " + j1.getNombre() + "!!");
         System.out.println("QUE DESEA HACER A CONTINUACION?");
-        elegir(j1, scan);
+        elegir(j1, cru);
 
     }
 
-    //Creamos la baraja
+    // Creamos la baraja
     public static void baraja() {
 
         String[] palos = {"Corazones", "Diamantes", "Tréboles", "Picas"};
@@ -39,10 +39,10 @@ public class Main {
         }
     }
 
-    //Funcion que permite elegir al jugador que hacer
-    public static void elegir(Jugadores j1, Scanner scan) {
+    // Funcion que permite elegir al jugador que hacer
+    public static void elegir(Jugadores j1, Jugadores crupier) {
         System.out.print("1. JUGAR\n2. VER DINERO\n3. SALIR\n->");
-        int decision = scan.nextInt();
+        int decision = in.nextInt();
         switch (decision) {
             case 1:
                 System.out.println("-------------------EMPIEZA EL JUEGO-------------------");
@@ -51,35 +51,90 @@ public class Main {
                         "ESPERE MIENTRAS EL GRUPIER DA LAS CARTAS\n" +
                         "------------------------------------------------------\n");
                 try {
-                    Thread.sleep(5000);//TIEMPO DE ESPERA PARA GENERAR REALISMO
+                    Thread.sleep(3000); // TIEMPO DE ESPERA PARA GENERAR REALISMO
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                jugada(scan,j1);
+                jugada(j1, crupier);
                 break;
             case 2:
                 System.out.println("------------------------------------------------------\n" +
                         "SU DINERO ACTUAL ES DE: $" + j1.getDinero()
                         + "\n------------------------------------------------------");
-                elegir(j1, scan);
+                elegir(j1, crupier);
             case 3:
                 System.exit(0);
         }
     }
 
     //JUEGO
-    public static void jugada(Scanner scan, Jugadores j1) {
-        Random random = new Random();
+    public static void jugada(Jugadores j1, Jugadores crupier) {
         System.out.print("Ingrese su apuesta\n->$");
-        int apuesta = scan.nextInt();//APUESTA REALIZADA
-        System.out.println(j1.getNombre());
-        int num = random.nextInt(53);
-        int num2 = random.nextInt(53);
+        int apuesta = in.nextInt(); // APUESTA REALIZADA
+        System.out.println("\n\n" + j1.getNombre() + "\t\t\t\t\t\t\t\tCRUPIER");
+        int num = ThreadLocalRandom.current().nextInt(52);
+        int num3 = ThreadLocalRandom.current().nextInt(52); // NUMERO CARTA CRUPIER
+        int num2 = ThreadLocalRandom.current().nextInt(52);
         Carta carta1 = cartasEnBaraja.get(num);
+        Carta carta3 = cartasEnBaraja.get(num3); // CARTA CRUPIER
         Carta carta2 = cartasEnBaraja.get(num2);
+        j1.agregarMano(carta1); // AGREGAMOS LAS CARTAS A LA MANO DEL JUGADOR
+        j1.agregarMano(carta2);
         System.out.println(carta1.getValor() + " de " + carta1.getPalo());
-        System.out.println(carta2.getValor() + " de " + carta1.getPalo());
-        System.out.println("TOTAL ->" + (carta1.getValorNumerico()+ carta2.getValorNumerico()));
-        System.out.println();
+        try {
+            Thread.sleep(3000); // TIEMPO DE ESPERA PARA GENERAR REALISMO
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\t\t\t\t\t\t\t\t" + carta3.getValor() + " de " + carta3.getPalo());
+        try {
+            Thread.sleep(3000); // TIEMPO DE ESPERA PARA GENERAR REALISMO
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(carta2.getValor() + " de " + carta2.getPalo());
+        try {
+            Thread.sleep(1000); // TIEMPO DE ESPERA PARA GENERAR REALISMO
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TOTAL: " + (carta1.getValorNumerico() + carta2.getValorNumerico()) + "\t\t\t\t\t\t\t" +
+                "TOTAL: " + carta3.getValorNumerico());
+
+        // SACAMOS LOS RESULTADOS DE LA APUESTA
+        if (sigJug(apuesta)>21){
+            System.out.printf("TE PASASTE!!!");
+        } else if(sigJug(apuesta)==21){
+            System.out.println("BLACKJACK!");
+            j1.setDinero(j1.getDinero()+apuesta);
+        } else{
+            if (sigJug(apuesta) > carta3.getValorNumerico()){
+                j1.setDinero(j1.getDinero()+(apuesta*2));
+            }else if(sigJug(apuesta) == carta3.getValorNumerico()){
+                j1.setDinero(j1.getDinero());
+            }else{
+                j1.setDinero(j1.getDinero()-apuesta);
+            }
+        }
     }
+
+    public static int sigJug(int apuesta) {
+        System.out.println("------------------------------------------------------\n" +
+                "1. QUEDARSE\n" +
+                "2. PEDIR\n" +
+                "3. DOBLAR\n" +
+                "->");
+        int decision = in.nextInt();
+        switch (decision){
+            case 1:
+                return apuesta;
+            case 2:
+                break;
+            case 3:
+                apuesta = apuesta * 2;
+                return apuesta;
+        };
+        return decision;
+    }
+
 }
